8f125d001fbbb897f9b9be06ed26e52af186203c
Daniel He <dhe30@stuy.edu>
2021-10-25 10:59:04 -0400

    yes

---
+++ 02-Triangle/Point.java
@@ -0,0 +1,26 @@
+public class Point{
+  private double x,y;
+  public Point(double X, double Y){
+    x=X;
+    y=Y;
+  }
+  public Point(Point p){
+    x= p.x;
+    y= p.y;
+  }
+  public double getX(){
+    return x;
+  }
+  public double getY(){
+    return y;
+  }
+  public static double distance(Point X, Point Y){
+    return Math.sqrt((X.x-Y.x)*(X.x-Y.x)+(X.y-Y.y)*(X.y-Y.y));
+  }
+  public double distanceTo(Point X){
+    return Math.sqrt((X.x-x)*(X.x-x)+(X.y-y)*(X.y-y));
+  }
+  public String toString(){
+    return "(" + x + ", " + y + ")";
+  }
+}
---
+++ 02-Triangle/Triangle.java
@@ -0,0 +1,11 @@
+public class Triangle{
+  private Point v1;
+  private Point v2;
+  private Point v3;
+  public Triangle(Point v11, Point v22, Point v33){
+    v1 = v11;
+    v2 = v22;
+    v3 = v33;
+  }
+  
+}
