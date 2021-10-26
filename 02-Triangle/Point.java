c3ac71c82d631dc0126a877b2eac684b21a9c384
Daniel He <dhe30@stuy.edu>
2021-10-26 10:59:26 -0400

    yes

--- 02-Triangle/Point.java
+++ 02-Triangle/Point.java
@@ -23,9 +23,17 @@ public class Point{
   public String toString(){
     return "(" + x + ", " + y + ")";
   }
+  public static boolean closeEnough(double a, double b){
+    return (Math.abs(a - b)/a)*100.0 <= 0.001;
+  }
+  public boolean equals(Point other){
+    return (closeEnough(x, other.x) && closeEnough(y, other.y));
+  }
   public static void main(String[] args){
     Point one = new Point(0.0, 2.0);
     Point two = new Point(0.0, -2.0);
+    Point three = new Point(0.00001, 0.00000999999);
+    Point four = new Point(0.00000999999, 0.00001);
     Triangle a = new Triangle(0.0,1.0,0.0,0.0,1.0,0.0);
     Triangle b = new Triangle(0.0,-1.0,0.0,0.0,-1.0,0.0);
     Triangle c = new Triangle(5.0,0.0,0.0,0.0,5.0,10.0);
@@ -49,5 +57,8 @@ public class Point{
     c.setVertex(1, two);
     System.out.println(c);
     System.out.println(c.getPerimeter());
+    //
+    System.out.println(closeEnough(0.00001, 0.00000999999));
+    System.out.println(three.equals(four));
   }
 }
