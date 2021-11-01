10c907c11f9c97d7e95b414fb49aa0057ac182f5
Daniel He <dhe30@stuy.edu>
2021-11-01 11:08:46 -0400

    yes

---
+++ 03-GetReal/RealNumber.java
@@ -0,0 +1,47 @@
+public class RealNumber{
+  private double value;
+
+  public RealNumber(double v){
+    value = v;
+  }
+
+  /*
+  *Return the sum of this and the other
+  */
+  public double add(RealNumber other){
+    return value + other;
+     //other can be ANY RealNumber, including a RationalNumber
+
+     //or other subclasses of RealNumber (that aren't written yet)
+  }
+
+  /*
+  *Return the product of this and the other
+  */
+  public double multiply(RealNumber other){
+    return value * other;
+  }
+
+  /*
+  *Return the this divided by the other
+  */
+  public double divide(RealNumber other){
+    return value / other;
+  }
+
+  /*
+  *Return the this minus the other
+  */
+  public double subtract(RealNumber other){
+    return value - other;
+  }
+
+
+  public double getValue(){
+    return value;
+  }
+
+  public String toString(){
+    return ""+value;
+  }
+}
