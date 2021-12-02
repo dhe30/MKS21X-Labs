84815cef8d2bbe16d965036aab55941150443293
Daniel He <dhe30@stuy.edu>
2021-12-01 21:20:38 -0500

    yes

--- 08-Sorts/Sorts.java
+++ 08-Sorts/Sorts.java
@@ -7,21 +7,25 @@ public class Sorts{
   */
   public static void bubbleSort(int[] data){
     int length = data.length;
-    int switched = 1;
-    while (switched != 0){
-      for (int a = 0; a < length - 1; a++){
+    boolean sorted = false;
+    while (!sorted){
+      boolean switched = false;
+      for (int a = 0; a < (length - 1); a++){
         if (data[a] > data[a + 1]){
           int h = data[a];
           data[a] = data[a + 1];
           data[a + 1] = h;
-          switched++;
+          switched = true;
         }
       }
-      length--
+      if (!switched){
+        sorted = true;
+      }
+      length--;
     }
   }
   public static void main (String[] args){
-    int[] a = new int[]{0, 1, 2, 1, 5, 3, 4};
+    int[] a = new int[]{0, 9,8,2,1,4,3,2,2,1,1, 2, 1, 5, 3, 4};
     bubbleSort(a);
     System.out.println(Arrays.toString(a));
   }
