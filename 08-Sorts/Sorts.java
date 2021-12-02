a65e46616101679a72cea727be48ba3b7661a852
Daniel He <dhe30@stuy.edu>
2021-12-02 10:55:40 -0500

    yes

--- 08-Sorts/Sorts.java
+++ 08-Sorts/Sorts.java
@@ -25,8 +25,105 @@ public class Sorts{
     }
   }
   public static void main (String[] args){
-    int[] a = new int[]{0, 9,8,2,1,4,3,2,2,1,1, 2, 1, 5, 3, 4};
-    bubbleSort(a);
-    System.out.println(Arrays.toString(a));
+    // MANY DUPLICATES
+    boolean win = true;
+    int max = 9;
+    int min = 7;
+    int length = 10000;
+    int[] sortTest = new int[length];
+    int[] bubbleTest = new int[length];
+    for (int i = 0; i < length; i++){
+      int value = (int)(Math.random()*(max - min + 1)) + min;
+      sortTest[i] = value;
+      bubbleTest[i] = value;
+    }
+    Arrays.sort(sortTest);
+    bubbleSort(bubbleTest);
+    // System.out.println(Arrays.toString(sortTest));
+    // System.out.println(Arrays.toString(bubbleTest));
+    for (int i = 0; i < length; i++){
+      if (bubbleTest[i] != sortTest[i]){
+        System.out.println("Index: " + i + " Value: " + bubbleTest[i] + " Actual Value: " + sortTest[i]);
+        win = false;
+      }
+    }
+    if (win){
+      System.out.println("Many duplicates Success");
+    }
+    // WITH NEGATIVES
+    win = true;
+    max = 1000;
+    min = -1000;
+    length = 10000;
+    sortTest = new int[length];
+    bubbleTest = new int[length];
+    for (int i = 0; i < length; i++){
+      int value = (int)(Math.random()*(max - min + 1)) + min;
+      sortTest[i] = value;
+      bubbleTest[i] = value;
+    }
+    Arrays.sort(sortTest);
+    bubbleSort(bubbleTest);
+    // System.out.println(Arrays.toString(sortTest));
+    // System.out.println(Arrays.toString(bubbleTest));
+    for (int i = 0; i < length; i++){
+      if (bubbleTest[i] != sortTest[i]){
+        System.out.println("Index: " + i + " Value: " + bubbleTest[i] + " Actual Value: " + sortTest[i]);
+        win = false;
+      }
+    }
+    if (win){
+      System.out.println("With negatives Success");
+    }
+    // WITH ONLY ONE VALUE
+    win = true;
+    max = 1;
+    min = 1;
+    length = 10000;
+    sortTest = new int[length];
+    bubbleTest = new int[length];
+    for (int i = 0; i < length; i++){
+      int value = (int)(Math.random()*(max - min + 1)) + min;
+      sortTest[i] = value;
+      bubbleTest[i] = value;
+    }
+    Arrays.sort(sortTest);
+    bubbleSort(bubbleTest);
+    // System.out.println(Arrays.toString(sortTest));
+    // System.out.println(Arrays.toString(bubbleTest));
+    for (int i = 0; i < length; i++){
+      if (bubbleTest[i] != sortTest[i]){
+        System.out.println("Index: " + i + " Value: " + bubbleTest[i] + " Actual Value: " + sortTest[i]);
+        win = false;
+      }
+    }
+    if (win){
+      System.out.println("Only one value Success");
+    }
+    // REVERSE SORT
+    win = true;
+    max = 1;
+    min = 1;
+    length = 10000;
+    sortTest = new int[length];
+    bubbleTest = new int[length];
+    for (int i = 0; i < length; i++){
+      int value = (int)(Math.random()*(max - min + 1)) + min;
+      sortTest[i] = value;
+      bubbleTest[i] = value;
+    }
+    int reverse = Arrays.sort(sortTest);
+    bubbleSort(bubbleTest);
+    // System.out.println(Arrays.toString(sortTest));
+    // System.out.println(Arrays.toString(bubbleTest));
+    for (int i = 0; i < length; i++){
+      if (bubbleTest[i] != sortTest[i]){
+        System.out.println("Index: " + i + " Value: " + bubbleTest[i] + " Actual Value: " + sortTest[i]);
+        win = false;
+      }
+    }
+    if (win){
+      System.out.println("Only one value Success");
+    }
   }
 }
