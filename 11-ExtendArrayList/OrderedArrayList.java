782fe232bdebaf367d3e3a9d199b78872f3b9710
Daniel He <dhe30@stuy.edu>
2021-12-19 15:22:59 -0500

    yes

--- 11-ExtendArrayList/OrderedArrayList.java
+++ 11-ExtendArrayList/OrderedArrayList.java
@@ -5,4 +5,29 @@ public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T
   public OrderedArrayList(int initialCapacity){
     super(initialCapacity);
   }
+  private int whereToPlace(T value){
+      /*return the index that the value should be placed
+      when inserting into the OrderedArrayList .*/
+      if (value == null){
+        throw new IllegalArgumentException("null");
+      }
+      for (int i = 0; i < size(); i++){
+        if (value.compareTo(get(i)) < 0){
+          return i;
+        }
+      }
+      return size();
+    }
+  public T set(int a, T b){
+    remove(a);
+    super.add(whereToPlace(b), b);
+    return b;
+  }
+  public boolean add(T b){
+    super.add(whereToPlace(b), b);
+    return true;
+  }
+  public void add(int a, T b){
+    add(b);
+  }
 }
--- 11-ExtendArrayList/tester.java
+++ 11-ExtendArrayList/tester.java
@@ -1,6 +1,16 @@
 public class tester{
   public static void main(String[] args){
+    try{
     NoNullArrayList<String> a = new NoNullArrayList<String>();
-    a.add(null);
+    a.add(0, null);
+  } catch (IllegalArgumentException e){
+    System.out.println("yes");
+  }
+    OrderedArrayList<Integer> b = new OrderedArrayList<Integer>();
+    for (int i = 10; i >= 0; i--){
+      b.add(i);
+    }
+    b.add(null);
+    System.out.println(b);
   }
 }
