49474ba14b80e369a7968631ce4a630eafd99eee
Daniel He <dhe30@stuy.edu>
2022-01-08 17:13:52 -0500

    created attack and implemented misses and no mana conditions

--- 12-Stuyablo/Wizard.java
+++ 12-Stuyablo/Wizard.java
@@ -6,7 +6,7 @@ public class Wizard extends Adventurer{
     this("Nick");
   }
   public Wizard(String name){
-    this(name, "I love you", 10);
+    this(name, "I love you", 20);
   }
   public Wizard(String name, String incantation, int mana){
     super(name,30+(int)(Math.random()*10));
@@ -19,4 +19,25 @@ public class Wizard extends Adventurer{
   public void setIncantation(String a){
     this.incantation = a;
   }
+  public int getMana(){
+    return mana;
+  }
+  public void attack(Damageable other){
+    int damage = 10 + (int)(Math.random()*16);
+    if(getMana() >= 10){
+      setMana(getMana() - 10);
+      if(((int)(Math.random()*10) + 1) <= 3){
+        System.out.println("Oops! " + this +" tripped on a rock!");
+      } else {
+        other.applyDamage(damage);
+        System.out.println(this + " casts a spell using 10 mana to deal " + damage + " damage!");
+      }
+    } else {
+      other.applyDamage(damage/4);
+      int self = (int)(Math.random()*(damage/4 + 1)) + 1;
+      applyDamage(self);
+      System.out.println(this + " has run out of mana! " + this + " uses black magic and sacrifices " + self + " HP to deal " + damage/4 +" damage!");
+    }
+  }
+  
 }
