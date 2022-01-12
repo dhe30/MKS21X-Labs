41e5dbcd17ce769c96fef67b78b2862c5a24b927
Daniel He <dhe30@stuy.edu>
2022-01-12 10:58:02 -0500

    implemented scanner but not functional yet

--- 12-Stuyablo/Game.java
+++ 12-Stuyablo/Game.java
@@ -18,7 +18,19 @@ public class Game{
   public static final int BOLD = 1;
   public static final int UNDERLINE = 4;
   public static final int INVERTED = 7;
-
+  public static void numbers(int[] a){
+    for (int i = 0; i < 4; i++){
+      int Science = a[i];
+      Text.go(2,2 + i*19);
+      if (Science < 25){
+        System.out.print(Text.colorize("" + Science, RED));
+      } else if (Science > 75){
+        System.out.print(Text.colorize("" + Science, GREEN));
+      } else {
+        System.out.print(Text.colorize("" + Science, WHITE));
+      }
+    }
+  }
   public static void did(){
     Text.hideCursor();
     Text.clear();
@@ -36,23 +48,18 @@ public class Game{
     for (int a = 0; a < 80; a++){
       System.out.print(Text.colorize(" ", WHITE + BACKGROUND));
     }
-    for (int i = 0; i < 4; i++){
-      int Science = (int)(Math.random()*100);
-      Text.go(2,2 + i*19);
-      if (Science < 25){
-        System.out.print(Text.colorize("" + Science, RED));
-      } else if (Science > 75){
-        System.out.print(Text.colorize("" + Science, GREEN));
-      } else {
-        System.out.print(Text.colorize("" + Science, WHITE));
-      }
-    }
+
     Text.go(31,1);
     Text.reset();
     Text.showCursor();
   }
   public static void main (String[] args){
     did();
+    int[] rando = new int[4];
+    for (int i = 0; i < 4; i++){
+      rando[i] = (int)(Math.random()*100);
+    }
+    numbers(rando);
     Scanner input = new Scanner(System.in);
     System.out.print(">");
 
